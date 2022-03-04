8c37f596aba1a5c663af12a1e9d934baa3b17685
Roman Odesiuk <roman.odesyuk@gmail.com>
2022-03-05 00:37:57 +0400

    Update ftr.sh

--- ftr.sh
+++ ftr.sh
@@ -14,11 +14,6 @@ pip3 install --upgrade pip
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
-agents_to_install.csv && \
-echo '"projects/ftr-express/zones/europe-west3-c/instances/instance-1","[{""type"":""ops-agent""}]"' >> agents_to_install.csv && \
-curl -sSO https://dl.google.com/cloudagents/mass-provision-google-cloud-ops-agents.py && \
-python3 mass-provision-google-cloud-ops-agents.py --file agents_to_install.csv
-
 mkdir ddos && cd ddos 
 
 git clone https://github.com/MHProDev/MHDDoS.git
